9f91b5aa0c8cf6ea4f47a6258a89a78c34b99f57
Ben Clark <benclark@Bens-MacBook-Pro.local>
2025-07-10 22:15:42 +1000

    Trying to add moving animation

--- src/app/projects/af-chatbot/page.tsx
+++ src/app/projects/af-chatbot/page.tsx
@@ -1,14 +1,14 @@
-// import ChatBox from "@/components/ui/ChatBox";
+import ChatBox from "@/components/ui/ChatBox";
 
-// export default function AFChatbotPage() {
-//   return (
-//     <main className="max-w-3xl mx-auto px-4 py-8">
-//       <h1 className="text-4xl font-bold mb-2">AF Chatbot</h1>
-//       <p className="mb-6 text-gray-600">
-//         A conversational assistant designed for Anytime Fitness clubs, helping members get fast, friendly, accurate answers to location-specific questions — including billing, hours, memberships, and more.
-//       </p>
+export default function AFChatbotPage() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-2">AF Chatbot</h1>
+      <p className="mb-6 text-gray-600">
+        A conversational assistant designed for Anytime Fitness clubs, helping members get fast, friendly, accurate answers to location-specific questions — including billing, hours, memberships, and more.
+      </p>
 
-//       <ChatBox />
-//     </main>
-//   );
-// }
+      <ChatBox />
+    </main>
+  );
+}
